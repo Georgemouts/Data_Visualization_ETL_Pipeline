8204e22bc073111b42b50d3eaddb6cd550e996ff
Giorgos Moutsopoulos <georgemuts@gmail.com>
2022-03-14 15:25:14 +0200

    Add files via upload

--- src/gui/UI.java
+++ src/gui/UI.java
@@ -1,55 +1,62 @@
 package gui;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
- 
 public class UI extends Application {
-	
-	
+	Stage window; 
+	Scene scene ; 
+	Button button;
+	ListView<String> listView;
     public static void main(String[] args) {
         launch(args);
     }
     
-    
-    
-    @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
+    	window = primaryStage;
+    	window.setTitle("the button");
+    	button = new Button("Submit");
+    	
+    	listView = new ListView<>();
+    	listView.getItems().addAll("Ena","dyo","tria");
+    	listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    	
+    	button.setOnAction(e->buttonClicked());
     	
     	
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-        	
-        	
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-      
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    	
+    	VBox layout = new VBox(10);
+    	layout.setPadding(new Insets(20,20,20,20));
+    	layout.getChildren().addAll(listView,button);
+    	
+    	scene = new Scene(layout,300,250);
+    	window.setScene(scene);
+    	window.show();
+    	
     }
+    
+    private void buttonClicked() {
+    	String message ="";
+    	ObservableList<String> movies;
+    	movies =listView.getSelectionModel().getSelectedItems();
+    	for(String m: movies) {
+    		message +=m +"\n";
+    	}
+    	System.out.println(message);
+    	
+    }
+   
 }
